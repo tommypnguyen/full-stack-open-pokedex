6ecff73b737cf5244972915614ed24bc5d136959
Tommy Nguyen <tommynguyen.164@gmail.com>
2023-09-06 21:05:27 -0700

    update for proper healthcheck

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('version', (req, res) => {
-  res.send('v1 is being deployed')
+  res.send('v2 is being deployed')
 })
 
 app.listen(PORT, () => {
