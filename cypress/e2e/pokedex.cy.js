2304d5341bb764c12f28e05925885113eabddc88
Tommy Nguyen <tommynguyen.164@gmail.com>
2023-09-06 20:06:50 -0700

    Add new cypress test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -3,5 +3,12 @@ describe('Pokedex', function() {
     cy.visit('')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('navigate to pokemon page', function() {
+    cy.visit('')
+    cy.visit('/pokemon/ivysaur')
+    cy.contains('ivysaur')
+    cy.contains('overgrow')
+    cy.contains('speed')
   })
 })
